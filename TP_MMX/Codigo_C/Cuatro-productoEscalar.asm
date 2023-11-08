a7d025330e6157faa21bb9395033119d0a750ddd
Giuliano1994 <71392131+Giuliano1994@users.noreply.github.com>
2023-11-08 18:59:42 -0300

    fix segmentation fault

--- TP_MMX/Codigo_C/Cuatro-productoEscalar.asm
+++ TP_MMX/Codigo_C/Cuatro-productoEscalar.asm
@@ -3,13 +3,19 @@ global productoEscalar
 
 productoEscalar:
     xor rax, rax              ; Inicializar rax en 0
-    movd xmm1, edi            ; Cargar el valor n en xmm1
+    movdqu xmm1, [edi]           ; Cargar el valor n en xmm1
     movdqu xmm2, [rsi]        ; Cargar el vector en xmm2 sin requerir alineación específica
 
     pmulld xmm2, xmm1         ; Multiplicar los elementos del vector por n (SSE2)
 
-    paddw xmm2, xmm2
-    movq rax, xmm2            ; Mover el resultado a rax
+    loop_start:
+    paddb xmm0, xmm2          ; Sumar el valor del contador a los elementos en mm0
+    add rdi, 8                ; Avanzar al siguiente segmento del vector
+    dec rsi                   ; Decrementar la longitud del vector
+
+    jnz loop_start
+   
+    movq rax, xmm0            ; Mover el resultado a rax
     ret
 
 
--- TP_MMX/Codigo_C/Cuatro-productoEscalar.c
+++ TP_MMX/Codigo_C/Cuatro-productoEscalar.c
@@ -3,8 +3,8 @@
 extern long long productoEscalar(long long *escalar, long long *vector);
 
 int main() {
-    long long vector[] = {1, 2, 3};
-    long long  escalar[]= {2, 2, 2};
+    long long vector[] = {1, 2, 3, 4};
+    long long  escalar[]= {2, 2, 2, 2};
 
     long long resultado = productoEscalar(escalar, vector);
 
